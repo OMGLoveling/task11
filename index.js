d4546c6283cf4bd73a695d4b2ffba13317ee72ff
wangyuxuan <742943632@qq.com>
2017-09-01 06:02:04 +0800

    task11

--- index.js
+++ index.js
@@ -1,1 +1,136 @@
-console.log("Hello World");
+const query = require('cli-interact');
+function cheackInformation(input){
+    let reg = new RegExp(/^[\u4e00-\u9fa5]+,\d+,\d+,\d+,\d+,\d+,\d+$/);
+   return reg.test(input);
+}
+function addStudent(information){
+    let str=information.split("，").join("：").split("：");
+    let result=[];
+    let a=str[0];
+    result.push({
+        name:a,
+        id:str[1],
+        grade:str[2],
+        subject: [
+            {key: '高数', score: 100},
+            {key: '数据库', score: 100},
+            {key: '英语', score: 100},
+            {key: 'java程序设计', score: 100}]
+    });
+    console.log(`学生${str[0]}的成绩被添加`);
+    return result;
+};
+function getScore(id,student){
+        let trans=[];
+        student.forEach(item=>{
+            let sum = item.subject[0].score + item.subject[1].score + item.subject[2].score +item.subject[3].score;
+            let average=sum/4;
+            if(item.id===id)
+            {
+                trans.push({
+                    name: item.name,
+                    id: item.id,
+                    math: item.subject[0].score,
+                    database: item.subject[1].score,
+                    english: item.subject[2].score,
+                    javaprogramming: item.subject[3].score,
+                    average: average,
+                    total: sum
+                });
+            }
+        });
+        return trans;
+}
+function getAve(student) {
+    let sum = [];
+    student.forEach(item => {
+        sum.push(item.subject[0].score);
+        sum.push(item.subject[1].score);
+        sum.push(item.subject[2].score);
+        sum.push(item.subject[3].score);
+    });
+
+    /*let total = sum.reduce((a, b) => {
+        return a + b;
+    });*/
+    let total=0;
+    for(let i = 0;i<sum.length;i++)
+    {
+        total+=sum[i];
+    }
+    let average = total / sum.length;
+    let Mid = getMid(sum);
+    return [average, Mid];
+}
+function getMid(sum) {
+    let halfPostion ;
+    sum.sort((a, b) => {
+        return a - b;
+    });
+
+    if (sum.length % 2 === 0) {
+        halfPostion = (sum[sum.length / 2] + sum[sum.length / 2 - 1])/ 2;
+    }else {
+        halfPostion = sum[(sum.length - 1) / 2];
+    }
+    return halfPostion;
+
+}
+
+function showScoreList(trans,student){
+
+    let title = "成绩单\n姓名|高数|数据库|英语|java程序设计|平均分|总分\n========================\n";
+    let mid = "";
+    let [average, Mid] = getAve(student);
+    trans.map(item => {
+        mid += `${item.name}|${item.math}|${item.database}|${item.english}|${item.javaprogramming}|${item.total}
+`;
+    });
+    let bottom = `========================
+全班总分平均数：${average}
+全班总分中位数：${Mid}
+`;
+    let result="";
+        result= title + mid + bottom;
+    console.log(result);
+
+}
+function main() {
+    console.log('1.添加学生\n2.生成成绩单\n3.退出\n');
+    let inputNum = query.getNumber('请输入你的选择（1～3）：');
+    console.log(inputNum);
+    let student=[];
+    if(inputNum===1)
+    {
+        let information = query.question('请输入学生信息（格式：姓名, 学号, 班级, 学科: 成绩, ...），按回车提交：');
+        if (cheackInformation(information)) {
+            console.log(' 请按正确的格式输入（格式：姓名, 学号, 班级, 学科: 成绩, ...）：');
+        }
+        student.push(addStudent(information));
+        return ;
+    }
+    else if(inputNum===2)
+    {
+        let id = query.question(`请输入要打印的学生的学号（格式： 学号, 学号,...），按回车提交：`);
+        if (id <= 0) {
+            console.log('请按正确的格式输入要打印的学生的学号（格式： 学号, 学号,...），按回车提交：')
+        }
+        let trans= getScore(id, student);
+        showScoreList(trans, student);
+        return;
+
+    }
+    else if (listNum == 3) {
+        console.log('正在退出....');
+        return;
+    }
+
+}
+main();
+module.exports = {
+    cheackInformation,
+    addStudent,
+    main,
+    getScore,
+    showScoreList,
+}
--- spec/main_spec.js
+++ spec/main_spec.js
@@ -1,39 +1,103 @@
 "use strict";
 var chai = require("chai");
+var sinon = require("sinon");
+var sinonChai = require("sinon-chai");
 var expect = chai.expect;
 
-describe("map reduce", function(){
-    before(function () {
-        Array.prototype.reduce = function(fn, initialValue) {
-            var result = initialValue;
-            for (var i in this) {
-                result = fn(result, this[i]);
-            }
-            return result;
-        };
+chai.use(sinonChai);
 
-        Array.prototype.map = function (fn) {
-            return this.reduce(function(result, item) {
-                result.push(fn(item));
-                return result;
-            }, []);
-        }
+
+var main = require("../index.js");
+
+
+describe("测试函数", function () {
+    it("测试输入学生信息是否正确", function () {
+        var input = "雨萱,001,1505,100,100,100,100";
+        var result = main.cheackInformation(input);
+        expect(result).to.equal(true);
     });
 
-    it("should reduce", function () {
-        var arr = [1, 2, 3];
-        var result = arr.reduce(function(sum, number) {
-            return sum + number;
-        }, 0);
-        expect(result).to.equal(6);
+    it("测试添加学生信息到列表", function () {
+        let input = "雨萱，001，1505，高数：100，数据库：100，英语：100，java程序设计：100";
+        let result = main.addStudent(input);
+        expect(result).to.deep.equal([{
+            name:"雨萱",
+            id:'001',
+            grade:'1505',
+            subject: [
+                {key: '高数', score: 100},
+                {key: '数据库', score: 100},
+                {key: '英语', score: 100},
+                {key: 'java程序设计', score: 100}]
+        }])
+    });
+    it('得到指定学生的信息',function() {
+        let input = [
+            {   name: "雨萱",
+                id: '001',
+                grade: '1505',
+                subject:[{key: '高数', score: 100},
+                    {key: '数据库', score: 100},
+                    {key: '英语', score: 100},
+                    {key: 'java程序设计', score: 100}
+                ]
+            },
+            {   name: "灵药",
+                id: '002',
+                grade: '1501',
+                subject:[
+                    {key: '高数', score: 60},
+                    {key: '数据库', score: 80},
+                    {key: '英语', score: 70},
+                    {key: 'java程序设计', score: 90}]
+            }];
+        let result = main.getScore('001',input);
+        expect(result).to.deep.equal([{
+            name: '雨萱',
+            id: '001',
+            math: 100,
+            database: 100,
+            english: 100,
+            javaprogramming: 100,
+            average: 100,
+            total: 400
+        }]);
+
     });
 
-    it("should map", function(){
-        var arr = [1, 2, 3];
-        var result = arr.map(function (number) {
-            return number * 2;
-        });
+    it('打印信息', () => {
+        let input = [
+            {   name: "雨萱",
+                id: '001',
+                grade: '1505',
+                subject:[{key: '高数', score: 100},
+                    {key: '数据库', score: 100},
+                    {key: '英语', score: 100},
+                    {key: 'java程序设计', score: 100}
+                ]
+            },
+            {   name: "灵药",
+                id: '002',
+                grade: '1501',
+                subject:[
+                    {key: '高数', score: 60},
+                    {key: '数据库', score: 80},
+                    {key: '英语', score: 70},
+                    {key: 'java程序设计', score: 90}]
+            }];
 
-        expect(result).to.deep.equal([2, 4, 6]);
-    });
+        let res = main.getScore('001',input);
+        main.showScoreList(res,input);
+        let except_text=
+ `
+成绩单
+姓名|高数|数据库|英语|java程序设计|平均分|总分
+========================
+雨萱|100|100|100|100|100|400
+========================
+全班总分平均数：87.5
+全班总分中位数：95`;
+        sinon.spy(console, 'log');
+        expect(console.log).to.be.calledWith(except_text);
+});
 });
